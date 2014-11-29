69f7be89b714c54c21db5146311c663a5a455463
AmbBAI <amb.main@gmail.com>
2014-11-29 18:59:25 +0800

    mesh

--- game_engine/component/ge_mesh.h
+++ game_engine/component/ge_mesh.h
@@ -4,6 +4,9 @@
 #include "common/ge_include.h"
 #include "common/ge_engine.h"
 #include "component/ge_component.h"
+#include "type/ge_vector3.h"
+#include "type/ge_vector2.h"
+#include "type/ge_color.h"
 
 namespace ge
 {
@@ -11,6 +14,14 @@ namespace ge
 class GE_API Mesh : Component
 {
 	DLL_MANAGE_CLASS(Mesh);
+
+protected:
+	std::vector<Vector3> verties;
+	std::vector<int> triangles;
+	std::vector<Vector3> normals;
+	std::vector<Color32> colors32;
+	std::vector<Vector2> uv;
+	std::vector<Vector2> uv2;
 };
 
 }
